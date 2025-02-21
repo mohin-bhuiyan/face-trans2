67cd4092d290714bc155ce89f63b1c5a829c3920
mohin-bhuiyan <mohinbhuiyan522@gmail.com>
2025-02-21 16:52:07 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let email= document.querySelector('input[type="text"]');
+let password= document.querySelector("input[type='password']");
+let butto = document.querySelector('#login');
+
+let audio= document.querySelector('audio');
+
+
+
+
+butto.addEventListener('click',(event)=>{
+    if(email.value!=="" && password.value !== ""){
+        event.preventDefault();
+
+        audio.currentTime= 0.2;
+        audio.play();
+
+       audio.onended = function () {
+            window.location.href = "deep3.html";
+        };
+
+    }else{
+        alert("fill the box please")
+    }
+    
+   
+
+})
+
+
+
